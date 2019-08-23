7c5b1b72d45c84386f01018aa6a31484f55a33e0
Oğuzhan GÜMÜŞ <40228440+prensmiskin@users.noreply.github.com>
2019-08-23 20:53:42 +0300

    s
    s

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php 
 
-$server="sql7.freemysqlhosting.net";
-$username="sql7302807";
-$password="iB6QcldJ2N";
-$dbname="sql7302807";
-try {
-	$conn = new PDO("mysql:host=$server;dbname=$dbname",$username,$password);
-	$conn ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-	echo "Veri tabanı bağlantısı başarılı";
-} catch (PDOException $e) {
-	echo "Hata Oluştu:" $e->getMessage();
-	
-}
 
+echo "sss";
  ?>
