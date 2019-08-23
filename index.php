9e7ee2cd2a0869ce9a6d639b44c0140f345951d7
Oğuzhan GÜMÜŞ <40228440+prensmiskin@users.noreply.github.com>
2019-08-23 21:16:38 +0300

    s
    s

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php 
 
-$servername = "ql7.freemysqlhosting.net";
-$username = "sql7302807";
-$password = "iB6QcldJ2N";
+$link = mysql_connect('ql7.freemysqlhosting.net', 'sql7302807', 'iB6QcldJ2N');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
 
 
 
