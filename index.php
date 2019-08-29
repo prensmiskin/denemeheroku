b80b7622e4f940fd7f4f0b5a48b94954cd86b314
Oğuzhan GÜMÜŞ <40228440+prensmiskin@users.noreply.github.com>
2019-08-29 16:03:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "hello word";
+$servername = "sql209.epizy.com";
+$username = "epiz_24131462";
+$password = "CIzewt7NzMGYGh";
+$dbname = "epiz_24131462_denemethree";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
