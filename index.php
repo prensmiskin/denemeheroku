dd1da119b8ecd3142c6d04cafd8e15440b18642b
Oğuzhan GÜMÜŞ <40228440+prensmiskin@users.noreply.github.com>
2019-09-01 09:30:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,5 @@
 <?php
 
-$servername = "localhost:3306";
-$username = "user";
-$password = "2092641182";
-$dbname = "deneme";
-	
-	
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$name=$_POST['kisi_ad'];
-	$ad=$_POST['kisi_tel'];
-	
-
-      
-
-
-
-	
-	
-   
-    // use exec() because no results are returned
-    
-
-	
-	
-	
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
-
-$conn = null;
-	
+echo "aaaaa";
 	
 ?>
